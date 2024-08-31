b8a84d86c0aa13e5e1c09161ba4c787c9c76f278
Anon Dev <michaelerastus9@gmail.com>
2024-08-31 19:48:16 +0100

    Added livechat

--- resources/views/home/base.blade.php
+++ resources/views/home/base.blade.php
@@ -454,10 +454,21 @@
         $(".notifier").stop(true).fadeIn(300);
         window.setTimeout(function() {
             $(".notifier").stop(true).fadeOut(300);
-        }, 6000);
+        }, 1200000);
         run = setInterval(request, interval);
     }
 </script>
-
+<!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+    var _smartsupp = _smartsupp || {};
+    _smartsupp.key = '244683a5af117884abd60bf5ab5bf0f277e434d6';
+    window.smartsupp||(function(d) {
+        var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+        s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+        c.type='text/javascript';c.charset='utf-8';c.async=true;
+        c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+    })(document);
+</script>
+<noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
 </body>
 </html>
--- resources/views/user/base.blade.php
+++ resources/views/user/base.blade.php
@@ -398,5 +398,17 @@
     new ClipboardJS('.copy');
 </script>
 @stack('js')
+<!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+    var _smartsupp = _smartsupp || {};
+    _smartsupp.key = '244683a5af117884abd60bf5ab5bf0f277e434d6';
+    window.smartsupp||(function(d) {
+        var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+        s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+        c.type='text/javascript';c.charset='utf-8';c.async=true;
+        c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+    })(document);
+</script>
+<noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
 </body>
 </html>
